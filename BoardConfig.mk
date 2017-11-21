532a9f75fccc3e1b1595dfb27825e562708b7b09
karthick111 <karthickrko61@gmail.com>
2017-11-21 15:45:41 +0100

    land: add multirom support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,10 +87,36 @@ RECOVERY_SDCARD_ON_DATA := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 
 # Disable Mouse Cursor
-#TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_INPUT_BLACKLIST := "hbtp_vm"
 
 # exFAT FS Support
 TW_INCLUDE_FUSE_EXFAT := true
 
 # NTFS Support
 TW_INCLUDE_FUSE_NTFS := true
+
+#MultiROM config. MultiROM also uses parts of TWRP config
+MR_INPUT_TYPE := type_b
+MR_DEV_BLOCK_BOOTDEVICE := true
+MR_INIT_DEVICES := $(LOCAL_PATH)/multirom/mr_init_devices.c
+MR_DEVICE_HOOKS_VER := 4
+MR_DEVICE_HOOKS := $(LOCAL_PATH)/multirom/mr_hooks.c
+MR_DPI := hdpi
+MR_DPI_FONT := 216
+DEVICE_RESOLUTION := 720x1280
+MR_PIXEL_FORMAT := "RGBA_8888"
+MR_ENCRYPTION := true
+MR_KEXEC_MEM_MIN := 0x0
+MR_USE_MROM_FSTAB := true
+MR_FSTAB := $(LOCAL_PATH)/multirom/mrom.fstab
+MR_CONTINUOUS_FB_UPDATE := true
+MR_USE_QCOM_OVERLAY := true
+MR_QCOM_OVERLAY_HEADER := $(LOCAL_PATH)/multirom/mr_qcom_overlay.h
+MR_NO_KEXEC := enabled
+TARGET_RECOVERY_IS_MULTIROM := true
+
+# Versioning
+include $(LOCAL_PATH)/multirom/MR_REC_VERSION.mk
+ifeq ($(MR_REC_VERSION),)
+MR_REC_VERSION := $(shell date -u +%Y%m%d)-01
+endif

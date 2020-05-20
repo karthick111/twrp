57e5b684226bd64443d4f0d41bff77261824d28e
bigbiff <bigbiff@teamw.in>
2020-05-20 10:15:34 -0400

    includes: update includes for base and ext4 policies
    Change-Id: I212eec2d4ce07b4b72a1c8582ed83f3f8a038a1e

--- partition.cpp
+++ partition.cpp
@@ -2459,6 +2459,7 @@ bool TWPartition::Wipe_Data_Without_Wiping_Media() {
 }
 
 bool TWPartition::Recreate_AB_Cache_Dir(const ext4_encryption_policy &policy) {
+#ifdef TW_INCLUDE_FBE
 	struct passwd pd;
 	struct passwd *pwdptr = &pd;
 	struct passwd *tempPd;
@@ -2508,6 +2509,7 @@ bool TWPartition::Recreate_AB_Cache_Dir(const ext4_encryption_policy &policy) {
 			}
 		}
 	}
+#endif
 	return true;
 }
 
